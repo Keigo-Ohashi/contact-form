c8b60772ce49d441deac7bfe3c98211c65e2841b
Keigo Ohashi <ex0084k@gmail.com>
2023-10-09 16:26:28 +0900

    2023/10/09

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ContactController::class, 'index']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts', [ContactController::class, 'store']);
